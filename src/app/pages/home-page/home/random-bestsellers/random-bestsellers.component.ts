5ae35c7914ef8b8757c8ff232802167ddd6146bf
Shannon-Amarasekara <shannon.amarasekara1@gmail.com>
2021-01-24 18:46:25 +0000

    basket display

--- src/app/pages/home-page/home/random-bestsellers/random-bestsellers.component.ts
+++ src/app/pages/home-page/home/random-bestsellers/random-bestsellers.component.ts
@@ -13,11 +13,7 @@ import { BookId } from 'src/app/core/models/book/book-id/bookid';
 export class RandomBestsellersComponent implements OnInit {
 
   fiveRandomBestsellers: Book[];
-  booksForDisplay: Book[];
   bookId: BookId;
-  firstBook: Book;
-  lastBook: Book;
-
 
   constructor(private bookService: BookService, private basketService: BasketService, private router: Router) { }
 
@@ -28,7 +24,6 @@ export class RandomBestsellersComponent implements OnInit {
   public findFiveRandomBestsellers(): void {
     this.bookService.findFiveRandomBestsellers().subscribe(books => {
       this.fiveRandomBestsellers = books;
-      this.booksForDisplay = books;
     })
 
 
@@ -42,12 +37,6 @@ export class RandomBestsellersComponent implements OnInit {
 
   public viewBook(id: BookId): void {
     this.router.navigateByUrl(`/books/${id.value}`);
-  }
-
-  public next(){
-    for (let index = 0; index < this.fiveRandomBestsellers.length - 1; index++) {
-      
-        this.booksForDisplay[index] = this.fiveRandomBestsellers[index+1];  }
   }
 
 }
